b83cf05231a24bbfa3dea9ed645b0b96736f906a
CellsGao <gshome6@gmail.com>
2017-12-13 19:26:21 +0800

    week 2 S3

--- example.py
+++ example.py
@@ -19,15 +19,16 @@ def fillUnivList(ulist, html):
     soup = BeautifulSoup(html, 'html.parser')
     for tr in soup.find('tbody').children:
         if isinstance(tr, bs4.element.Tag):
-            tds = tr('td')
+            tds = tr.find_all('td')
             ulist.append([tds[0].next, tds[1].string, tds[3].string])
 
 
 def printUnivList(ulist, num):
-    print("{:^10}\t{:^6}\t{:^10}".format("排名", "学校名称", "总分"))
+    tplt = '{0:10}\t{1:{3}^10}\t{2:^6}'
+    print(tplt.format("排名", "学校名称", "总分",chr(12288)))  # 数字表宽度
     for i in range(num):
         u = ulist[i]
-        print("{:^10}\t{:^6}\t{:^10}".format(u[0], u[1], u[2]))
+        print(tplt.format(u[0], u[1], u[2], chr(12288)))
 
 
 def main():
