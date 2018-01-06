11d8561c3a03ce1bdac2d8dc5a96b5b4e0f67c17
CellsGao <gshome6@gmail.com>
2018-01-06 17:23:12 +0800

    1

---
+++ stockSpider.py
@@ -0,0 +1,24 @@
+import requests
+import re
+import traceback
+from bs4 import BeautifulSoup
+
+
+def getHtmlText(url):
+    return ''
+
+
+def getStockList(list, stcokUrl):
+    return ''
+
+
+def printList(list):
+    return ''
+
+
+def getStockInfo(list, StockUrl, fpath):
+    return ''
+
+
+def main():
+    stock_list_url=''
--- tbSpider.py
+++ tbSpider.py
@@ -1,43 +1,51 @@
 import requests
 import re
 
+
 def getHtmlText(url):
     try:
-        r=requests.get(url)
+        r = requests.get(url)
         r.raise_for_status()
-        r.encoding=r.apparent_encoding
+        r.encoding = r.apparent_encoding
         return r.text
     except:
         print('getHtmlText函数出现错误')
 
-def parseHtml(list,html):
+
+def parseHtml(list, html):
     try:
-        tlt=re.findall(r'\"raw_title\":\".*？\"',html)
-        plt=re.findall(r'\"view_price\":\"[1-9]\d*.\d*|0.\d*[1-9]\d*\"',html)
-        for i in len(plt):
-            price=eval(plt[i].split(':')[1])
-            title=eval(tlt[i].split(':')[1])
-            list.append([price,title])
+        tlt = re.findall(r'\"raw_title\"\:\".*？\"', html)
+        plt = re.findall(r'\"view_price\"\:\"[1-9]\d*.\d*', html)
+        for i in range(len(plt)):
+            price = eval(plt[i].split(':')[1])
+            title = eval(tlt[i].split(':')[1])
+            list.append([price, title])
     except:
         print('parseHtml函数出现错误')
 
 
-
 def printList(list):
-    print('')
+    tplt = '{:4}\t{:8}\t{:16}'
+    print(tplt.format('序号', '价格', '名称'))
+    count = 0
+    for g in list:
+        count += 1
+        print(tplt.format(count, g[0], g[1]))
+
 
 def main():
     try:
         start_url = 'https://s.taobao.com/search?q='
         goods = input('请输入要搜索的物品')
         depth = input('请输入要搜索的页面数')
-        list=[]
+        list = []
         for i in range(int(depth)):
             text = getHtmlText(start_url + goods + '&s=' + str(44 * int(depth)))
             parseHtml(list, text)
     except ValueError:
         print('输入数据类型错误')
     printList(list)
+
 
 if __name__ == '__main__':
     main()
